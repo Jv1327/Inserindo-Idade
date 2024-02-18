e2f3da0370e3250848f9c10243c156511cf68e0e
Jv1327 <jvrodrigues132770@gmail.com>
2024-02-18 08:31:53 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    int idade = 0;
+
+    printf("Valor Inicial da Idade: %d.\n", idade);
+    printf("Digite uma idade:\n");
+    scanf("%d", &idade);
+
+    printf("Idade Informada: %d.\n", idade);
+}
